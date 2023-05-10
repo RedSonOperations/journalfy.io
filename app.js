5e6ab7043683558726e68612ccc4237e989c5650
RedSonOperations <alecmkain@gmail.com>
2023-05-10 17:28:45 -0400

    Added MongoDB to store posts, updated style rendering

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash=require("lodash");
+const mongoose=require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/blogDB', {useNewUrlParser: true});
+
+const postSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    body: { type: String, required: true }
+  });
+
+const Post=new mongoose.model("Post", postSchema);
+
+module.exports=Post;
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -9,7 +20,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-let posts=[];
+
 
 app.set('view engine', 'ejs');
 
@@ -17,7 +28,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    res.render("home", {homeContent: homeStartingContent, postsArr: posts });
+    Post.find({}).then(function(posts){
+        res.render("home", {homeContent: homeStartingContent, postsArr: posts});
+    })
 })
 
 app.get("/about", function(req, res){
@@ -35,30 +48,32 @@ app.get("/compose", function(req, res){
 app.post("/compose", function(req, res){
     var postContent=req.body.postContent;
     var postTitle=req.body.postTitle;
-    const post={
+    const post= new Post({
         title: postTitle,
         body: postContent
-    }
-    
-    posts.push(post);
+    });
+    post.save();
     res.redirect("/");
 })
 
-app.get("/posts/:postName", function(req, res){
-    const route=lodash.lowerCase(req.params.postName);
-
-    posts.forEach(function(post){
-        const postTitle=lodash.lowerCase(post.title);
-
-        if(route===postTitle){
-            res.render("post", {postTitleEJS: post.title, postContentEJS: post.body});
-        }
-
-    })
+app.get("/posts/:postId", function(req, res){
+    const postId = req.params.postId;
     
-})
+    Post.findOne({_id: postId}).then(function(post){
+        if (post) {
+            res.render("post", {postTitleEJS: post.title, postContentEJS: post.body});
+        } else {
+            console.log(postId);
+            res.status(404).send("Post not found");
+        }
+    }).catch(function(err){
+        console.log(err);
+        res.status(500).send("Error retrieving post");
+    });
+});
 
 
-app.listen(3000||process.env.PORT, function() {
+
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
